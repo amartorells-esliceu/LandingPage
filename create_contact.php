d164a382790afe9584a820e823c32f1e3316f169
Your Name <you@example.com>
2024-05-31 10:58:53 +0200

    Hola Mundo

--- create_contact.php
+++ create_contact.php
@@ -1,4 +1,5 @@
 <?php
+include 'db.php';
 
 if($_SERVER['REQUEST_METHOD']=="POST"){
     echo "CREANDO CONTACTO NUEVO <BR>";
@@ -7,11 +8,22 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     $correo=$_POST['correo'];
     $mensaje=$_POST['mensaje'];
     
-    
+
     echo "NOMBRE: " .$nombre. "<br>";
     echo "TELEFONO: " .$telefono. "<br>";
     echo "CORREO: " .$correo. "<br>";
     echo "MENSAJE: " .$mensaje. "<br>";
+    sql="INSERT INTO contactos (nombre,telefono,correo,mensaje) VALUES ('$nombre','$telefono','$correo','$mensaje')";
+    echo $sql. "<br>";
+    $stmt = $conn->prepare($sql);    
+    $result = $stmt->execute();
+
+    if($result){
+        echo "CONTACTO CREADO CON EXITO";
+    }else{
+        echo "ERROR AL CREAR CONTACTO";
+    }
+
 }else{
     echo "ERROR EN METODO POST";
 }
