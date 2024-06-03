377080cd7573a7bd6839d10479ac64ce46c63c4a
Your Name <you@example.com>
2024-06-03 08:41:17 +0200

    Hola Mundo

--- create_contact.php
+++ create_contact.php
@@ -13,18 +13,24 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     echo "TELEFONO: " .$telefono. "<br>";
     echo "CORREO: " .$correo. "<br>";
     echo "MENSAJE: " .$mensaje. "<br>";
-    sql="INSERT INTO contactos (nombre,telefono,correo,mensaje) VALUES ('$nombre','$telefono','$correo','$mensaje')";
+    $sql="INSERT INTO contactos (nombre,telefono,correo,mensaje) VALUES (?,?,?,?)";
     echo $sql. "<br>";
-    $stmt = $conn->prepare($sql);    
+    $stmt = $conn->prepare($sql);
+    $stmt ->bind_param("ssss", $nombre, $telefono, $correo, $mensaje);    
     $result = $stmt->execute();
 
     if($result){
+        $stmt ->close();
+        $conn ->close();
         echo "CONTACTO CREADO CON EXITO";
+        header("Location: contacto.php?status=success&msg=Mensage Guardado!");
+
     }else{
         echo "ERROR AL CREAR CONTACTO";
     }
 
+
+    
 }else{
     echo "ERROR EN METODO POST";
 }
-.
